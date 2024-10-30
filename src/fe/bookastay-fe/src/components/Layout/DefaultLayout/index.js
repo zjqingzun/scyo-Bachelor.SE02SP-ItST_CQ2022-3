65d0a743877af5755ef94bdd1d50adbc8a715ad1
Hoang Le Nam <hoanglenam0905@gmail.com>
2024-10-30 16:26:53 +0700

    initial fe base

---
+++ src/fe/bookastay-fe/src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import Footer from "./Footer";
+
+const DefaultLayout = ({ children }) => {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <div className="content">{children}</div>
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default DefaultLayout;
---
+++ src/fe/bookastay-fe/src/pages/Register/index.js
@@ -0,0 +1,5 @@
+const Register = () => {
+    return <h2>Register page</h2>;
+};
+
+export default Register;

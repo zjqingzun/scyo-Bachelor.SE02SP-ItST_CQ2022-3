e103883caec591909ea4fae4c262f0460d6e87e4
22120243 <ankiney2004@gmail.com>
2024-11-22 20:17:00 +0700

    volume for persist data

--- src/be/be_nestjs/setup.ps1
+++ src/be/be_nestjs/setup.ps1
@@ -1,16 +1,19 @@
 docker run --name postgres_bookastay `
   -e POSTGRES_USER=bookastaydata `
   -e POSTGRES_PASSWORD=bookastaydata `
-  -v ./pgdata:/var/lib/postgresql/data `
+  -v ${PWD}\pgdata:/var/lib/postgresql/data `
   -p 5432:5432 `
   -d postgres
 
 Start-Sleep -Seconds 10
 
+docker exec -it postgres_bookastay psql -U bookastaydata -c 'CREATE DATABASE "bookastay" WITH ENCODING = ''UTF8'' TEMPLATE = template0;'
+
+
 docker run -d --name minio `
   -p 9000:9000 `
   -p 9001:9001 `
-  -v ./minio:/data `
+  -v ${PWD}\minio:/data `
   -e "MINIO_ACCESS_KEY=bookastayimage" `
   -e "MINIO_SECRET_KEY=bookastayimage" `
   minio/minio server /data --console-address ":9001"

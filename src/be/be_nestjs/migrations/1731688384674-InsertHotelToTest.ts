b561a20baf18b3148b168f46b5a8df2e8ef4f052
22120243 <ankiney2004@gmail.com>
2024-11-22 15:40:13 +0700

    change docker

---
+++ src/be/be_nestjs/migrations/1731688384674-InsertHotelToTest.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class  InsertHotelToTest1731688384674 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

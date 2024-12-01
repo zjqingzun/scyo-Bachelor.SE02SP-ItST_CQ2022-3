3ba48d04e9ed35a2326ca5a13423fdf05e3062d6
22120217 <hoanglenam0905@gmail.com>
2024-12-01 18:18:10 +0700

    add step progress bar component

---
+++ src/fe/bookastay-fe/src/components/Stepper/Stepper.js
@@ -0,0 +1,78 @@
+import { useEffect, useRef, useState } from "react";
+import "./Stepper.scss";
+
+const Stepper = ({
+    stepsConfig = [],
+    currentStep = 1,
+    setCurrentStep = () => {},
+    isComplete = false,
+    setIsComplete = () => {},
+}) => {
+    const stepRef = useRef([]);
+
+    const [margins, setMargins] = useState({
+        marginLeft: 0,
+        marginRight: 0,
+    });
+
+    const calculateProgressBarWidth = () => {
+        return ((currentStep - 1) / (stepsConfig.length - 1)) * 100;
+    };
+
+    useEffect(() => {
+        setMargins({
+            marginLeft: stepRef.current[0].offsetWidth / 2,
+            marginRight: stepRef.current[stepsConfig.length - 1].offsetWidth / 2,
+        });
+    }, [stepRef, stepsConfig.length]);
+
+    if (stepsConfig.length === 0) {
+        return <> </>;
+    }
+
+    return (
+        <>
+            <div className="stepper">
+                {stepsConfig.map((step, index) => {
+                    const isCurrent = index + 1 === currentStep;
+                    const isCompleted = index + 1 < currentStep || isComplete;
+                    return (
+                        <div
+                            ref={(el) => (stepRef.current[index] = el)}
+                            key={index}
+                            className={`step ${isCurrent ? "step--current" : ""} ${
+                                isCompleted ? "step--completed" : ""
+                            }`}
+                            onClick={() => {
+                                if (isCompleted) {
+                                    setCurrentStep(index + 1);
+                                }
+                            }}
+                        >
+                            <div className="step__title">{step.title}</div>
+                            <div className="step__number">{index + 1}</div>
+                        </div>
+                    );
+                })}
+
+                <div
+                    className="progress-bar"
+                    style={{
+                        width: `calc(100% - ${margins.marginLeft + margins.marginRight}px)`,
+                        marginLeft: margins.marginLeft,
+                        marginRight: margins.marginRight,
+                    }}
+                >
+                    <div
+                        className="progress-bar__progress"
+                        style={{
+                            width: `${calculateProgressBarWidth()}%`,
+                        }}
+                    ></div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Stepper;
--- src/fe/bookastay-fe/src/pages/Reserve/Reserve.js
+++ src/fe/bookastay-fe/src/pages/Reserve/Reserve.js
@@ -1,9 +1,89 @@
+import { useState } from "react";
 import "./Reserve.scss";
 
+import Stepper from "~/components/Stepper/Stepper";
+
 const Reserve = () => {
+    const stepsConfig = [
+        {
+            title: "Your selection",
+        },
+        {
+            title: "Your details",
+        },
+        {
+            title: "Payment details",
+        },
+    ];
+
+    const [currentStep, setCurrentStep] = useState(1);
+    const [isComplete, setIsComplete] = useState(false);
+
+    const handleNext = () => {
+        setCurrentStep((prevStep) => {
+            if (prevStep === stepsConfig.length) {
+                setIsComplete(true);
+                return prevStep;
+            }
+
+            return prevStep + 1;
+        });
+    };
+
+    const handlePrev = () => {
+        if (currentStep > 1) {
+            setCurrentStep(currentStep - 1);
+        }
+    };
+
     return (
         <div>
             <h1>Reserve</h1>
+
+            <Stepper
+                stepsConfig={stepsConfig}
+                currentStep={currentStep}
+                setCurrentStep={setCurrentStep}
+                isComplete={isComplete}
+                setIsComplete={setIsComplete}
+            />
+
+            <div className="content">
+                {currentStep === 1 && (
+                    <div className="step-content">
+                        <h2>Your selection</h2>
+                        <p>Content for step 1</p>
+                    </div>
+                )}
+
+                {currentStep === 2 && (
+                    <div className="step-content">
+                        <h2>Your details</h2>
+                        <p>Content for step 2</p>
+                    </div>
+                )}
+
+                {currentStep === 3 && (
+                    <div className="step-content">
+                        <h2>Payment details</h2>
+                        <p>Content for step 3</p>
+                    </div>
+                )}
+            </div>
+
+            <div className="actions">
+                {!isComplete && currentStep > 1 && (
+                    <button className="btn btn-secondary" onClick={() => handlePrev()}>
+                        Prev
+                    </button>
+                )}
+
+                {!isComplete && (
+                    <button className="btn btn-success" onClick={() => handleNext()}>
+                        {currentStep === stepsConfig.length ? "Finish" : "Next"}
+                    </button>
+                )}
+            </div>
         </div>
     );
 };

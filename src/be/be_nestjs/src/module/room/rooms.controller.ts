3bb1a209c5be2346c507a84d3721991f3abe8e6a
Sakate Kataro <IS14professional@hotmail.com>
2025-01-10 13:16:17 +0700

    add totalOccupied booking/dashboard

--- src/be/be_nestjs/src/module/room/rooms.controller.ts
+++ src/be/be_nestjs/src/module/room/rooms.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { RoomsService } from './rooms.service';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
+import { Public } from '@/helpers/decorator/public';
 
 @Controller('rooms')
 export class RoomsController {
